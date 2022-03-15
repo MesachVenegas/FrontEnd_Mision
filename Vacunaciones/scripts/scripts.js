e97a65c66dd04c7343cd2484f3f3df0dafde7617
Mesach Venegas <99062079+MesachVenegas@users.noreply.github.com>
2022-03-14 20:44:46 -0700

    change: move files to new repository

--- Vacunaciones/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-const toggler = document.getElementById('toggler')
-
-toggler.addEventListener('click', (e)=>{
-    console.log(e.target.classList.contains('toggler-title'));{
-        console.log(e.target.parentElement)
-    }
-})
